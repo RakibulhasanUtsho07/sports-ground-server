9418deebf0b8b8de77e603917d9e94afea662544
rakibulhasanutsho <162662167+rakibulhasanutsho@users.noreply.github.com>
2026-05-18 16:25:26 +0600

    complete home page feachered section and all facilities page

--- index.js
+++ index.js
@@ -1,37 +1,57 @@
-const express = require("express")
-const dotenv = require("dotenv")
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
 
-const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
-PORT= process.env.PORT
-dotenv.config()
-app.get("/", (req, res )=>{
-    res.send("Server is running")
-})
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
-const uri = process.env.MONGODB_URI
+app.use(cors());
+app.use(express.json());
+dotenv.config();
+PORT = process.env.PORT;
+const uri = process.env.MONGODB_URI;
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
 
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 async function run() {
   try {
-    
     await client.connect();
+    const db = client.db("matchday");
+    const groundCollection = db.collection("grounds");
+
+    app.get("/grounds", async (req, res) => {
+      const result = await groundCollection.find().toArray();
+      res.send(result);
+    });
+    app.get("/grounds/:id", async (req, res) => {
+        const {id} = await req.params
+      const result = await groundCollection.findOne({
+        _id: new ObjectId(id)
+      }
+
+      )
+      res.send(result);
+    });
+
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
   } finally {
-    
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-app.listen(PORT, ()=>{
-    console.log(`Server running on  port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server running on  port ${PORT}`);
+});
