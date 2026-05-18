adeb08b17196d18aac31d8d5cc2a25a10dde7dd9
rakibulhasanutsho <162662167+rakibulhasanutsho@users.noreply.github.com>
2026-05-18 07:48:36 +0600

    setup mongodb

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const express = require("express")
+const dotenv = require("dotenv")
+
 const app = express()
-PORT= 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+PORT= process.env.PORT
+dotenv.config()
+app.get("/", (req, res )=>{
+    res.send("Server is running")
+})
 
+const uri = process.env.MONGODB_URI
 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    
+    await client.connect();
 
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(PORT, ()=>{
     console.log(`Server running on  port ${PORT}`)
